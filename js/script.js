c43312f60fd132792bc9fc2cf1ac306c48a044fa
David Boxler <david.boxler@incluit.com>
2022-08-10 19:19:04 -0300

    se sube proyecto primera parte

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/*=============== BTN MENU HEADER ===============*/
+let navbar = document.querySelector(".header .navbar");
+let menuBtn = document.querySelector("#menu-btn");
+
+menuBtn.onclick = () => {
+  menuBtn.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+/*=============== SHOW HOME SLIDER ===============*/
+var swiper = new Swiper(".home-slider", {
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".carousel-button.next",
+    prevEl: ".carousel-button.prev",
+  },
+});
+
+/*=============== SHOW HOME SLIDER ===============*/
+var swiper = new Swiper(".promotion-slider", {
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".carousel-button.next",
+    prevEl: ".carousel-button.prev",
+  },
+});
---
+++ services/form.php
@@ -0,0 +1,22 @@
+<?php
+
+$nombre = $_POST['name'];
+$localidad = $_POST['local'];
+$email = $_POST['email'];
+$telefono = $_POST['phone'];
+
+// Cuerpo del email para la compañia
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su email es " . $email . ",\r\n";
+$mensaje .= "Su Localidad es " . $localidad . ",\r\n";
+$mensaje .= "Mensaje " . $_POST['mensaje'] . ",\r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'companiapizzas@gmail.com';
+$asunto = 'Sistema de franquiciado';
+
+mail($para, $asunto, utf8_decode($mensaje), $header)
+
+$header('Location:services/exito.html');
+
+?>
